e34e7d4f85090e3638327e4edbe4703bdfe49277
sjaak <sjaakkantvcreations@gmail.com>
2020-03-08 22:06:56 +0100

    reworked code to something I find easier to work with

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 let currentQuestion = 0;
 let votes = [];
 
-function createElement(elementType, appendTo, id, innerText) {
+function createElement(elementType, appendTo, id, innerText, properties = {}) {
     let element = document.createElement(elementType);
     document.getElementById(appendTo).appendChild(element);
     element.id = id;
@@ -12,33 +12,41 @@ function createElement(elementType, appendTo, id, innerText) {
 
 document.getElementById("startButton").onclick = function () {
     this.remove();
-    nextQuestion(currentQuestion);
-    createElement("button", "BTNcontainer", "BTNeens", "mee eens");
-    document.getElementById("BTNeens").onclick = function () {
-        nextQuestion(currentQuestion);
-        votes.push("true");
-    };
-    createElement("button", "BTNcontainer", "BTNgeenMening", "geen mening");
-    document.getElementById("BTNgeenMening").onclick = function () {
-        votes.push("no opinion");
-        nextQuestion(currentQuestion);
-    };
-    createElement("button", "BTNcontainer", "BTNoneens", "niet mee eens");
-    document.getElementById("BTNoneens").onclick = function () {
-        votes.push("false");
-        nextQuestion(currentQuestion);
-    };
-};
-
-
-function nextQuestion(Question) {
-    console.log(currentQuestion);
-    console.log(subjects.length);
-    if (currentQuestion === subjects.length) {
-        alert("monkaS");
-    } else {
-        document.getElementById("title").innerHTML = subjects[currentQuestion].title;
-        document.getElementById("statement").innerHTML = subjects[currentQuestion].statement;
-        currentQuestion += 1;
+    document.getElementById("title").innerHTML = subjects[0].title;
+    document.getElementById("statement").innerHTML = subjects[0].statement;
+    let txt = ["Terug", "Sla deze vraag over", "Eens", "Oneens", "Geen mening"];
+    for (let i=0; i<5; i++) {
+        createElement("button", "BTNcontainer", "btn" + i, txt[i]);
+        if(i<=4 && i>=1){
+            document.getElementById("btn" + i).onclick = function(){onclickBTNsetup(i)};
+            function onclickBTNsetup(index) {
+                for (var a=1; a<5; a++){
+                    if(votes[index] !== txt[a]){
+                        votes.push(txt[index]);
+                        break
+                    }else {
+                        votes[index] = txt[index];
+                    }
+                }
+                if (currentQuestion === subjects.length) {
+                    alert("show answers");
+                } else {
+                    document.getElementById("title").innerHTML = subjects[currentQuestion].title;
+                    document.getElementById("statement").innerHTML = subjects[currentQuestion].statement;
+                    currentQuestion += 1;
+                }
+            }
+        }else if(i===0){
+            document.getElementById("btn" + i).onclick = function(){
+                if(currentQuestion>0){
+                    currentQuestion-=1;
+                    document.getElementById("title").innerHTML = subjects[currentQuestion].title;
+                    document.getElementById("statement").innerHTML = subjects[currentQuestion].statement;
+                }
+            }
+        }
+        // document.getElementById("btn0").onclick = function () {
+        //
+        // }
     }
-}
+};
