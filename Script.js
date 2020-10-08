104c2824adb49e0a52d64ae33f9adde5f8a5ac28
sjaak <kantsjaak@gmail.com>
2020-10-08 10:59:09 +0200

    Big Progress
    fixed point system, downloaded the data file to fix an error in the file and fixed some minor code.

--- Script.js
+++ Script.js
@@ -6,6 +6,7 @@ let weight = [];
 let txt = ["terug", "skip", "pro", "none", "contra"];
 let txtBTN = ["terug", "Sla deze vraag over", "Eens", "Geen mening", "Oneens"];
 //this holds the end scores
+let person = {'VVD':0, 'CDA':0, 'PVV':0, 'D66':0, 'GroenLinks':0, 'SP':0, 'PvdA':0, 'ChristenUnie':0, 'Partij voor de Dieren':0, 'SGP':0, 'DENK':0, 'Forum voor Democratie':0, 'Lokaal in de Kamer':0, 'OndernemersPartij':0, 'VNL':0, 'Nieuwe Wegen':0, 'De Burger Beweging':0, 'Piratenpartij':0, 'Artikel 1':0, 'Libertarische Partij':0, '50Plus':0, 'Vrijzinnige Partij':0, 'Niet Stemmers':0};
 let scores = [{
     name: "VVD",
     points: 0
@@ -73,9 +74,6 @@ let scores = [{
     name: "Vrijzinnige Partij",
     points: 0
 }, {
-    name: "Libertarische Partij",
-    points: 0
-}, {
     name: "Niet Stemmers",
     points: 0
 }];
@@ -128,6 +126,7 @@ document.getElementById("startButton").onclick = function () {
                     } else {
                         votes[currentQuestion] = txt[i];
                     }
+                    resetButtons();
                 }
                 currentQuestion++;
                 if (currentQuestion >= subjects.length) {
@@ -141,6 +140,7 @@ document.getElementById("startButton").onclick = function () {
             document.getElementById("btn" + i).onclick = function () {
                 if (currentQuestion > 0) {
                     currentQuestion--;
+                    blueQuestionButton(votes[currentQuestion]);
                     document.getElementById("title").innerHTML = subjects[currentQuestion].title;
                     document.getElementById("statement").innerHTML = subjects[currentQuestion].statement;
                 } else if (currentQuestion === 0) {
@@ -195,7 +195,7 @@ function changeOnclickCheckmark(i) {
 function partiesPickList() {
     document.getElementById("feedbackContainer").style.display = "none";
     document.getElementById("title").innerHTML = "Welke partijen wilt u meenemen in het resultaat?";
-    document.getElementById("statement").innerHTML = "U kunt kiezen voor grote partijen(deze hebben minimaal " + zetels + "). Ook kunt u kiezen of u alleen seculiere partijen wilt zien.";
+    document.getElementById("statement").innerHTML = "U kunt kiezen voor grote partijen(deze hebben minimaal " + zetels + " zetels). Ook kunt u kiezen of u alleen seculiere partijen wilt zien.";
     document.getElementById("NextButton").onclick = function () {endCalculation()};
     createElement("BUTTON", "voteContainer", "btnPartiesHuge", "grote partijen", "btnPartiesHuge");
     document.getElementById("btnPartiesHuge").onclick = function (){
@@ -209,6 +209,32 @@ function partiesPickList() {
     };
 }
 
+function resetButtons() {
+    for (var i = 2; i <= 4; i++) {
+        document.getElementById("btn" + i).style.backgroundColor = "black";
+
+    }
+}
+
+function blueQuestionButton(answer) {
+    resetButtons();
+    switch (answer) {
+        case "pro":
+            document.getElementById("btn2").style.backgroundColor = "#01B4DC";
+            break;
+        case "none":
+            document.getElementById("btn3").style.backgroundColor = "#01B4DC";
+            break;
+        case "contra":
+            document.getElementById("btn4").style.backgroundColor = "#01B4DC";
+            break;
+    }
+}
+
+function removeSpaces (str) {
+    return str.replace(/\s/g, '')
+}
+
 function endCalculation() {
     document.getElementById("title").innerHTML = "Hier ziet u de partijen die passen bij uw keuzes";
     document.getElementById("statement").style.display = "none";
@@ -216,52 +242,41 @@ function endCalculation() {
     document.getElementById("btnPartiesSmall").style.display = "none";
     document.getElementById("NextButton").style.display = "none";
     for (let i=0; i<subjects.length; i++){
+        console.log("question " + i);
         for (let x=0; x<subjects[i].parties.length; x++){
             if(votes[i] === "skip"){
+                console.log("skipped");
+                break;
+            }else if (votes[i] === "none"){
+                person["Niet Stemmers"] += parseInt(weight[i]);
                 break;
             }else if(subjects[i].parties[x].position === votes[i]){
-                scores[x].points += parseInt(weight[i]);
+                console.log("added " + weight[i] + " to " + console.log(subjects[i].parties[x].name) + " cause their position is " + subjects[i].parties[x].position);
+                person[subjects[i].parties[x].name] += parseInt(weight[i])
             }
         }
     }
+    createElement("DIV", "container", "answerWrapper");
     showList();
-    createElement("DIV", "content", "answerWrapper");
-    scores.sort( predicateBy("points") );
 }
 
 function showList() {
     if(partiesShown === "none"){
-        for (let x=scores.length-1; x>0; x--){
-            createElement("P", "answerWrapper", "answer" + scores[x].name, scores[x].name + " heeft " + scores[x].points + " punten op basis van uw keuzes.");
+        for (let x=0; x<scores.length; x++){
+            createElement("P", "answerWrapper", "answer" + parties[x].name, parties[x].name + " heeft " + person[parties[x].name] + " punten op basis van uw keuzes.");
         }
     }else if(partiesShown === "BigParties"){
-        /*let x=scores.length-1;
-        for (let i=0; i<parties.length; i++){
-            if (parties[i].secular===false){
+        for (let x=scores.length-1; x>0; x--){
+            if (parties[x].secular === false || parties[x].secular !== undefined) {
                 createElement("P", "answerWrapper", "answer" + scores[x].name, scores[x].name + " heeft " + scores[x].points + " punten op basis van uw keuzes.");
-            }
-            x--;
-        }*/
-        let x=0;
-        for (let i=0; i<parties.length; i++){
-            if (parties[i].secular === true && parties[i].secular !== undefined){
-                console.log(parties[i]);
-                console.log(scores[i]);
-                scores.splice(x,1);
-            }else {
-                x++;
             }
         }
     }else if(partiesShown === "SecunParties"){
-        if (parties[i].secular === true && parties[i].secular !== undefined){
-            console.log(parties[i]);
-        }
-        /*let x=scores.length-1;
-        for (let i=0; i<parties.length; i++){
-            if (parties[i].secular===true){
-                createElement("P", "answerWrapper", "answer" + scores[x].name, scores[x].name + " heeft " + scores[x].points + " punten op basis van uw keuzes.");
+        scores.sort( predicateBy("points") );
+        for (let x=23; x>0; x--){
+            if(true){
+                createElement("P", "answerWrapper", "answer" + scores[x].name, scores[x].name + " heeft " + scores[x].points + " punten, " + "");
             }
-            x--;
-        }*/
+        }
     }
 }
